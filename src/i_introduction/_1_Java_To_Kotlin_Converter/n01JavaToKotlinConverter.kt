a95d01b2ef13a210fb67e217b1292c028a4fb79f
Zhansong Li <zhansong.li@ir.com>
2018-08-10 16:40:04 +1000

    starting work on 0-4 chap i

--- src/i_introduction/_1_Java_To_Kotlin_Converter/n01JavaToKotlinConverter.kt
+++ src/i_introduction/_1_Java_To_Kotlin_Converter/n01JavaToKotlinConverter.kt
@@ -2,6 +2,21 @@ package i_introduction._1_Java_To_Kotlin_Converter
 
 import util.TODO
 
+fun doTask1(collection: Collection<Int>): String {
+    val sb = StringBuilder()
+    sb.append("{")
+    val iterator = collection.iterator()
+    while (iterator.hasNext()) {
+        val element = iterator.next()
+        sb.append(element)
+        if (iterator.hasNext()) {
+            sb.append(", ")
+        }
+    }
+    sb.append("}")
+    return sb.toString()
+}
+
 fun todoTask1(collection: Collection<Int>): Nothing = TODO(
     """
         Task 1.
@@ -12,7 +27,6 @@ fun todoTask1(collection: Collection<Int>): Nothing = TODO(
     """,
     references = { JavaCode1().task1(collection) })
 
-
 fun task1(collection: Collection<Int>): String {
-    todoTask1(collection)
+    return doTask1(collection)
 }
