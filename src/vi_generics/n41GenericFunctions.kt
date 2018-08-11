d408e3d480f73a5ed63db0a48e8e98a2a8d4920d
Zhansong Li <lizhansong@hvariant.com>
2018-08-11 23:24:56 +1000

    last chapter

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -19,12 +19,19 @@ fun task41(): Nothing = TODO(
         }
 )
 
+fun <T, C: Collection<T>, MC: MutableCollection<T>> C.partitionTo(c1: MC, c2: MC, predicate: (T) -> Boolean) :Pair<C, C> {
+    val (left, right) = partition(predicate)
+    left.toCollection(c1)
+    right.toCollection(c2)
+
+    // MutableCollection<T> implements Collection<T>
+    return Pair<C,C>(first=c1 as C,second=c2 as C)
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList<String>()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet<Char>()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
